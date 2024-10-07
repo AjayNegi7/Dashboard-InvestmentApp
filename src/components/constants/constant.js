af2dd3b49f6297c3a7c9956663485c8039b876a4
Ajay Negi <ajay.negi@CELSYSWTC.IN>
2024-10-07 12:11:58 +0530

    Constants

---
+++ src/components/constants/constant.js
@@ -0,0 +1,17 @@
+import DeveloperModeOutlinedIcon from '@mui/icons-material/DeveloperModeOutlined';
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import Brightness5OutlinedIcon from '@mui/icons-material/Brightness5Outlined';
+
+export const Image_URL1="https://img.freepik.com/premium-vector/financial-business-chart-with-moving-up-arrow-graph-stock-market-blue-color-background_293525-3566.jpg";
+export const Image_URL2="https://framerusercontent.com/images/ah45VMWYJx8bkgJTpVdXeNWCeA.png?scale-down-to=1024";
+export const Image_URL3="https://apperito.com/assets/images/image_solutions2_2x.png";
+
+export const icons = {
+    Brightness5OutlinedIcon,
+    DeveloperModeOutlinedIcon,
+    NotificationsNoneOutlinedIcon,
+    CloseOutlinedIcon,
+};
+
+export const navArray = ["DashBoard", "Build", "Launch", "Payments", "Project1", "Profile"];
